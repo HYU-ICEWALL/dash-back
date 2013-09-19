e7383e7e32956e3371aad62b106909e19711bd8a
neo91 <neo91@ChopServer.(none)>
2013-09-20 06:04:49 +0900

    session

--- dash/__init__.py
+++ dash/__init__.py
@@ -1,39 +1,52 @@
+# -*- coding: utf-8 -*- 
 #from flask import render_template
 
 #    return render_template('index.html', name='World')
-from flask import Flask, request, json, Response, make_response
+from flask import Flask, request, json, Response, make_response, abort, jsonify
 from models import User
 from database import db_session
+from beaker.middleware import SessionMiddleware
+
+session_opts = {
+	'session.type' : 'ext:memcached',
+	'session.url' : '127.0.0.1:11211',
+	'session.data_dir' : './cache'
+}
+
 app = Flask(__name__)
+app.secret_key = "/D/s)8'~!>W$%914<S_T#7)8Q8O(|i"
+app.wsgi_app = SessionMiddleware(app.wsgi_app, session_opts)
 
 @app.route('/')
 def root():
-	if User().checkDup(request.args.get('id', '')) :
-		return 'True'
-	else:
-		return 'False'
+	return 'hello'
 
 @app.route('/api/users', methods=['POST'])
 def do_the_join():
-	password = request.json['password']
-	major = request.json['major']
-	email = request.json['email']
-	fb_id = request.json['fb_id']
-	u = User(email, password, fb_id, major)	
+	try:
+		password = request.json['password']
+		major = request.json['major']
+		email = request.json['email']
+		fb_id = request.json['fb_id']
+		u = User(email, password, fb_id, major)	
+		
+		if ( u.checkDup() == True ) :
+			abort(409)
 	
-	if ( u.checkDup(email) == True ) :
-		return Response('', status=409)
-
-	if ( len(password) == 0 or len(email) == 0 ):
-		return Response('', status=400)
+		if ( len(password) == 0 or len(email) == 0 ):
+			abort(400)
 	
-	db_session.add(u)
-	db_session.commit()
+		db_session.add(u)
+		db_session.commit()
 
-	resp = make_response('', 201)
-	resp.headers['Location'] = '/api/users/me'
+		resp = make_response('', 201)
+		resp.headers['Location'] = '/api/users/me'
 
-	return resp
+		return resp
+
+	except:
+		abort(400)
+
 
 @app.route('/api/login', methods=['POST'])
 def do_login():
@@ -41,10 +54,42 @@ def do_login():
 	password = request.form['password']
 	u = User(uname, password)
 	
-	if ( u.checkLogin(uname, password) == True ) :
+	if ( u.checkLogin() == True ) :
+		session = request.environ['beaker.session']
+		print session
 		resp = make_response('', 200)
 		resp.headers['Location'] = '/api/users/me'
+		session['user'] = u.getUser()
+		session.save()
+		print session
 	else :
-		resp = make_response('', 404)
+		abort(404)
 
 	return resp
+
+@app.route('/api/users/me', methods=['GET'])
+def view_info():
+	u = isLogin()
+	
+	if u == None:
+		abort(404)
+	
+	return jsonify(u)
+
+
+
+
+
+def isLogin():
+	session = request.environ['beaker.session']
+	
+	if 'user' in session:
+		u = session['user']
+	else: 
+		return None
+		
+	return u
+
+
+
+
--- dash/models.py
+++ dash/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey
 import datetime
 from sqlalchemy.sql import select
 from database import Base
+import hashlib
 
 class User(Base):
 	__tablename__ = 'users'
@@ -13,26 +14,51 @@ class User(Base):
 	
 	def __init__(self, username='', password='', fid='', major=''):
 		self.username = username
-		self.password = password
+		self.password = hashlib.sha256(password).hexdigest()
 		self.fid = fid
 		self.major = major
-		
-		
+	
+	def getUser(self):
+		dic = {}
+		dic['id'] = self.idx
+		dic['email'] = self.username
+		dic['fb_id'] = self.fid
+		dic['major'] = self.major
+
+		return dic
+			
 	def __repr__(self):
 		return "User (%s, %s, %s, %s)" % (self.username, self.password, self.fid, self.major)
 
 
-	def checkDup(self, uname):
-		usr = self.query.filter(User.username==uname).first()
+	def checkDup(self):
+		usr = self.query.filter(User.username==self.username).first()
 		if usr == None:
 			return False
 		
 		return True
 
-	def checkLogin(self, uname, pwd):
-		usr = self.query.filter(User.username==uname, User.password==pwd).first()
-
+	def checkLogin(self):
+		usr = self.query.filter(User.username==self.username, User.password==self.password).first()
 		if usr == None:
 			return False
+		self.username = usr.username
+		self.fid = usr.fid
+		self.major = usr.major
+		self.idx = usr.idx
+		self.password = '00000000'
 
 		return True
+	def is_authenticated(self) :
+		return True
+
+	def is_active(self):
+		return True
+
+	def is_anonymous(self):
+		return False
+
+	def get_id(self):
+		return unicode(self.idx)
+
+	
